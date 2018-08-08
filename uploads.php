8cc61dc3234822f41def10454183ca9730b04940
tejas <tejasmht88@gmail.com>
2018-08-08 11:37:52 +0530

    PFJ-14, Added new upload folder and also added php file upload code.

---
+++ uploads.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_FILES['files']) && !empty($_FILES['files'])) {
+    $no_files = count($_FILES["files"]['name']);
+    for ($i = 0; $i < $no_files; $i++) {
+        if ($_FILES["files"]["error"][$i] > 0) {
+            echo "Error: " . $_FILES["files"]["error"][$i] . "<br>";
+        } else {
+            if (file_exists('uploads/' . $_FILES["files"]["name"][$i])) {
+                echo 'File already exists';                
+            } else {
+                $newfilename = round(microtime(true));
+                move_uploaded_file($_FILES["files"]["tmp_name"][$i], 'uploads/' . $newfilename.'_'.$_FILES["files"]["name"][$i]);
+                //echo 'File successfully uploaded : uploads/' . $_FILES["files"]["name"][$i] . ' ';
+            }
+        }
+    }
+}
