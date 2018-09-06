da5f7a06641975d5bb2183d2c9425b2ee5bf9a73
tejas <tejasmht88@gmail.com>
2018-09-06 17:20:26 +0530

    PFJ-23, File upload aspect ratio added for upload image.

--- uploads.php
+++ uploads.php
@@ -10,10 +10,32 @@ if (isset($_FILES['files']) && !empty($_FILES['files'])) {
             if (file_exists('uploads/' . $_FILES["files"]["name"][$i])) {
                 echo 'File already exists';                
             } else {
-                $newfilename = round(microtime(true));
+                $newfilename = round(microtime(true));                
                 move_uploaded_file($_FILES["files"]["tmp_name"][$i], 'uploads/' . str_replace(' ','-',$newfilename.'_'.$_FILES["files"]["name"][$i]));                
                 $files[$i]['Name']=str_replace(' ','-',$newfilename.'_'.$_FILES["files"]["name"][$i]);
                 $files[$i]['url']=$actual_link.'/uploads/'.str_replace(' ','-',$newfilename.'_'.$_FILES["files"]["name"][$i]);
+                if(isset($_POST['from_config']) && $_POST['from_config'] == 'yes'){
+                    $image_info = getimagesize($files[$i]['url']);
+                    $image_width = $image_info[0];
+                    $image_height = $image_info[1];                    
+                    if($image_width > 250 || $image_height > 80){
+                        $ext = pathinfo($_FILES["files"]["name"][$i], PATHINFO_EXTENSION);
+                        $newwidth = 250;
+                        $newheight = ceil(( $image_height / $image_width) * $newwidth);
+                        if($ext == 'jpg'){                            
+                            $src = imagecreatefromjpeg($files[$i]['url']);
+                            $dst = imagecreatetruecolor($newwidth, $newheight);
+                            imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $image_width, $image_height);
+                            imagejpeg($dst, __DIR__.'/uploads/'.str_replace(' ','-',$newfilename.'_'.$_FILES["files"]["name"][$i]),100);
+                        }
+                        if($ext == 'png'){
+                            $src = imagecreatefrompng($files[$i]['url']);
+                            $dst = imagecreatetruecolor($newwidth, $newheight);
+                            imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $image_width, $image_height);
+                            imagepng($dst, __DIR__.'/uploads/'.str_replace(' ','-',$newfilename.'_'.$_FILES["files"]["name"][$i]), 100);
+                        }
+                    }                    
+                }
             }
         }
     }
